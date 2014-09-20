263ec5364b049d5e269931daf05a7d560e6bd693
Mathias Grimm <mathiasgrimm@gmail.com>
2014-09-20 13:23:14 +0100

    doc

--- src/X.php
+++ src/X.php
@@ -1,142 +1,36 @@
 <?php
+/**
+ * X (https://github.com/mathiasgrimm/x)
+ *
+ * @author Mathias Grimm <mathiasgrimm@gmail.com>
+ */
 namespace mathiasgrimm\x;
 
 class X
 {
-    private static $isDebug = false;
+    private static $bIsDebug = false;
+    
+    public function __construct($bIsDebug = true, $bRegisterFunctions = true)
+    {
+        self::$bIsDebug = $bIsDebug;
+        
+        if ($bRegisterFunctions) {
+            self::registerFunctions();
+        }
+    }
     
     public static function setDebug($isDebug)
     {
-        self::$isDebug = $isDebug;
+        self::$bIsDebug = $isDebug;
     }
     
     public static function getDebug()
     {
-        return self::$isDebug;
+        return self::$bIsDebug;
     }
     
-    public static function x()
+    public static function registerFunctions()
     {
-        if (!self::$isDebug) {
-            return;
-        }
-    
-        echo ob_get_clean();
-    
-        $bCli = php_sapi_name() == 'cli' ? true : false;
-        $sNl = $bCli ? "\n" : "<br />";
-    
-        $aDbt = debug_backtrace();
-        $sFile = $aDbt[0]['file'];
-        $sLine = $aDbt[0]['line'];
-    
-        $aArgs = func_get_args();
-    
-        if (!$bCli) {
-            echo "<div style='text-align:left; border:2px solid;padding:10px;background:#096;'>";
-        }
-    
-        echo "DEBUG X {$sNl}";
-        echo "File: {$sFile} {$sNl}";
-        echo "Line: {$sLine} {$sNl}";
-    
-        if (!$bCli) {
-            echo "<pre>";
-        }
-    
-        foreach ($aArgs as $iIdx => $aArg) {
-            if ($bCli) {
-                echo $sNl;
-            } else {
-                echo "<div style='border:1px dotted;padding:10px;'>";
-            }
-    
-            echo "ARG [$iIdx] {$sNl}";
-            print_r($aArg);
-    
-            if (!$bCli) {
-                echo "</div>";
-            }
-    
-            echo "{$sNl}";
-        }
-    
-        if (!$bCli) {
-            echo "</pre>";
-            echo "</div>";
-        }
-    
-        echo "{$sNl}{$sNl}";
-    }
-    
-    public static function xd()
-    {
-        if (!self::$isDebug) {
-            return;
-        }
-    
-        echo ob_get_clean();
-    
-        $bCli = php_sapi_name() == 'cli' ? true : false;
-        $sNl = $bCli ? "\n" : "<br />";
-    
-        $aDbt = debug_backtrace();
-        $sFile = $aDbt[0]['file'];
-        $sLine = $aDbt[0]['line'];
-    
-        $aArgs = func_get_args();
-    
-        if ($bCli) {
-            echo $sNl;
-        } else {
-            echo "<div style='text-align:left; border:2px solid;padding:10px;background:#BBB;z-index:90000;'>";
-        }
-    
-        echo "DEBUG XD {$sNl}";
-        echo "File: {$sFile} {$sNl}";
-        echo "Line: {$sLine} {$sNl}";
-    
-        if (!$bCli) {
-            echo "<pre>";
-        }
-    
-        foreach ($aArgs as $iIdx => $aArg) {
-            if (!$bCli) {
-                echo "<div style='border:1px dotted;padding:10px;'>";
-            } else {
-                echo $sNl;
-            }
-    
-            echo "ARG [$iIdx] {$sNl}";
-            print_r($aArg);
-    
-            if (!$bCli) {
-                echo "</div>";
-            }
-    
-            echo "{$sNl}";
-        }
-    
-        if (!$bCli) {
-            echo "</pre>";
-            echo "</div>";
-        }
-    
-        echo "{$sNl}{$sNl}";
-        die();
-    }
-    
-    public static function registerAliases()
-    {
-        if (function_exists('x')) {
-            throw new \Exception('Function \'x\' already exists');
-        } else {
-            function x() 
-            {
-                call_user_func_array('X::x', func_get_args());
-            }
-        }
-
-            
+        require 'function.php';
     }
 }
---
+++ src/function.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * X (https://github.com/mathiasgrimm/x)
+ *
+ * @author Mathias Grimm <mathiasgrimm@gmail.com>
+ */
+use mathiasgrimm\x\X;
+
+function x()
+{
+    if (!X::getDebug()) {
+        return;
+    }
+    
+    echo ob_get_clean();
+    
+    $bCli = php_sapi_name() == 'cli' ? true : false;
+    $sNl = $bCli ? "\n" : "<br />";
+    
+    $aDbt = debug_backtrace();
+    $sFile = $aDbt[0]['file'];
+    $sLine = $aDbt[0]['line'];
+    
+    $aArgs = func_get_args();
+    
+    if (!$bCli) {
+        echo "<div style='text-align:left; border:2px solid;padding:10px;background:#096;'>";
+    }
+    
+    echo "DEBUG X {$sNl}";
+    echo "File: {$sFile} {$sNl}";
+    echo "Line: {$sLine} {$sNl}";
+    
+    if (!$bCli) {
+        echo "<pre>";
+    }
+    
+    foreach ($aArgs as $iIdx => $aArg) {
+        if ($bCli) {
+            echo $sNl;
+        } else {
+            echo "<div style='border:1px dotted;padding:10px;'>";
+        }
+        
+        echo "ARG [$iIdx] {$sNl}";
+        print_r($aArg);
+        
+        if (!$bCli) {
+            echo "</div>";
+        }
+        
+        echo "{$sNl}";
+    }
+    
+    if (!$bCli) {
+        echo "</pre>";
+        echo "</div>";
+    }
+    
+    echo "{$sNl}{$sNl}";
+}
+
+function xd()
+{
+    if (!X::getDebug()) {
+        return;
+    }
+    
+    echo ob_get_clean();
+    
+    $bCli = php_sapi_name() == 'cli' ? true : false;
+    $sNl = $bCli ? "\n" : "<br />";
+    
+    $aDbt = debug_backtrace();
+    $sFile = $aDbt[0]['file'];
+    $sLine = $aDbt[0]['line'];
+    
+    $aArgs = func_get_args();
+    
+    if ($bCli) {
+        echo $sNl;
+    } else {
+        echo "<div style='text-align:left; border:2px solid;padding:10px;background:#BBB;z-index:90000;'>";
+    }
+    
+    echo "DEBUG XD {$sNl}";
+    echo "File: {$sFile} {$sNl}";
+    echo "Line: {$sLine} {$sNl}";
+    
+    if (!$bCli) {
+        echo "<pre>";
+    }
+    
+    foreach ($aArgs as $iIdx => $aArg) {
+        if (!$bCli) {
+            echo "<div style='border:1px dotted;padding:10px;'>";
+        } else {
+            echo $sNl;
+        }
+        
+        echo "ARG [$iIdx] {$sNl}";
+        print_r($aArg);
+        
+        if (!$bCli) {
+            echo "</div>";
+        }
+        
+        echo "{$sNl}";
+    }
+    
+    if (!$bCli) {
+        echo "</pre>";
+        echo "</div>";
+    }
+    
+    echo "{$sNl}{$sNl}";
+    die();
+}
+
+function j()
+{
+    if (!X::getDebug()) {
+        return;
+    }
+    
+    echo ob_get_clean();
+    
+    $aDbt = debug_backtrace();
+    
+    $aData = array(
+        'sFile' => $aDbt[0]['file'],
+        'sLine' => $aDbt[0]['line'],
+        'aArgs' => func_get_args()
+    );
+    
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($aData);
+    die();
+}
